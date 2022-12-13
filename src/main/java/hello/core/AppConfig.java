5e5b6b73067ebca22df77b39dce7f3097523be2d
wlsah0814 <wlsah0814@naver.com>
2022-12-13 19:22:38 +0900

    3일차 내용 커밋(DI를 스프링 컨테이너를 이용하여 코드 변경)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,25 +9,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 애플리케이션의 설정 정보를 의미
 public class AppConfig {
 
+    @Bean // spring container에 등록된다.
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 new MemoryMemberRepository(),
                 discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+          return new RateDiscountPolicy();
     }
 
 }
